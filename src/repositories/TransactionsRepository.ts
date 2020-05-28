c6f6da481dedcf0202638e5a8429b4c4aede2fd5
Lucas <lucasmorais582@gmail.com>
2020-05-28 15:31:14 -0300

    Criação do método de leitura de arquivos csv

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,11 +1,27 @@
+/* eslint-disable func-names */
 import { EntityRepository, Repository } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 
 import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+interface ResponseObject {
+  transactions: TransactionDTO[];
+  categories: string[];
 }
 
 @EntityRepository(Transaction)
@@ -34,6 +50,39 @@ class TransactionsRepository extends Repository<Transaction> {
     };
 
     return balance;
+  }
+
+  public async loadCSV(filePath: string): Promise<ResponseObject> {
+    const readCSVStream = fs.createReadStream(filePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const transactions: TransactionDTO[] = [];
+    const categories: string[] = [];
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value || !category)
+        throw new AppError('This document is invalid.');
+
+      transactions.push({ title, type, value, category });
+      categories.push(category);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    await fs.promises.unlink(filePath);
+    return { transactions, categories };
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,31 +1,55 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
-
+import multer from 'multer';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionFromCsvService from '../services/CreateTransactionFromCsvService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import uploadConfig from '../config/upload';
 
 const transactionRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
-  const transactions = await transactionsRepository.find();
+  const transactions = await transactionsRepository.find({
+    select: ['id', 'title', 'value', 'type', 'created_at', 'updated_at'],
+    relations: ['category'],
+  });
   const balance = await transactionsRepository.getBalance();
 
-  return response.json({ transaction: transactions, balance });
+  return response.json({ transactions, balance });
 });
 
 transactionRouter.post('/', async (request, response) => {
-  const { title, value, type, category_id } = request.body;
+  const { title, value, type, category } = request.body;
   const createTransaction = new CreateTransactionService();
 
   const transaction = await createTransaction.execute({
     title,
     value,
     type,
-    category_id,
+    category,
   });
 
   return response.json(transaction);
+});
+
+transactionRouter.post(
+  '/from-csv',
+  upload.single('csv'),
+  async (request, response) => {
+    const transactionFromCsv = new CreateTransactionFromCsvService();
+    const transactions = await transactionFromCsv.execute(request.file.path);
+    return response.json(transactions);
+  },
+);
+
+transactionRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+  const deleteTransaction = new DeleteTransactionService();
+  await deleteTransaction.execute(id);
+  return response.json('This transaction has been deleted.');
 });
 
 export default transactionRouter;
